a1b982c50d3e9cf1d319d60e40aa11fad785d8b6
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-17 14:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let a in sampleObject){
+		if(a==key) return true;
+	}
+	return false;
 }
 
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+return false;
